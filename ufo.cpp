5579700d54825d5b0d26644474b892d4576b1d5a
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2023-02-09 23:31:52 +0000

    added while loop that displays incorrect guesses and the state of the game

--- ufo.cpp
+++ ufo.cpp
@@ -6,10 +6,19 @@ int main() {
     std::string codeword = "codecademy";
     std::string answer = "__________";
     int misses = 0;
+    std::vector<char> incorrect;
+    bool guess = false;
 
     greet();
 
     while(answer != codeword && misses < 7) {
+        display_misses(misses);
+        std::cout << "Incorrect Guesses: ";
+
+        for(char i : incorrect) {
+            std::cout << i;
+        }
+
         misses++;
     }
 
