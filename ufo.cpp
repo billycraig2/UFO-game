818c55d812dc89728ff9b180afd16ca17e9f64d8
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2023-02-10 00:04:19 +0000

    finished by adding user input

--- ufo.cpp
+++ ufo.cpp
@@ -8,13 +8,35 @@ int main() {
     int misses = 0;
     std::vector<char> incorrect;
     bool guess = false;
+    char letter;
 
     greet();
 
     while(answer != codeword && misses < 7) {
         display_misses(misses);
         display_status(incorrect, answer);
-        misses++;
+
+        std::cout << "Please enter your guess: ";
+        std::cin >> letter;
+
+        for(int i = 0; i < codeword.length(); i++) {
+            if(letter == codeword[i]) {
+                answer[i] = letter;
+                guess = true;
+            }
+        }
+
+        if(guess) {
+            std::cout << "Correct!\n";
+        }
+        else {
+            std::cout << "Incorrect! The tractor beam pulls the person in further.";
+            incorrect.push_back(letter);
+            misses++;
+        }
+
+        guess = false;
+
     }
 
     end_game(answer, codeword);
